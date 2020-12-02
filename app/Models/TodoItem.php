6174bd71d0eb559d45ed8aa054c12c8e8ded814a
Jake Vivian <jacob@sadsoft.com>
2020-12-01 19:43:10 -0800

    scoping todo items to user

--- app/Models/TodoItem.php
+++ app/Models/TodoItem.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Scopes\UserScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class TodoItem extends Model
 {
     use HasFactory;
+
+    protected static function booted()
+    {
+        static::creating(
+            function ($model) {
+                if(is_null($model->user_id)) {
+                    $model->user_id = auth()->user()->id;
+                }
+
+                return $model;
+            }
+        );
+        static::addGlobalScope(new UserScope);
+    }
 }
---
+++ database/migrations/2020_12_02_024219_add_user_id_column_to_todo_items.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserIdColumnToTodoItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('todo_items', function (Blueprint $table) {
+            $table->foreignIdFor(User::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('todo_items', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+}
